4465f1794b4a458e66c362278563c1ad6482e9f3
addweb <kajal.pa.addweb@gmail.com>
2021-12-23 17:01:15 +0530

    weather app

---
+++ weather_app/config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL:"https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:"33ad56e25a85ff64d4982ed6117c5454"
+    }
+}
+
+module.exports = constants;
